ceb62a83a068158003013fa9152635e90384a294
pava <pavahalis@protonmail.ch>
2021-06-21 12:47:49 +0300

    "Metallica", "Sex Pistols", "System Of A Down" and "The Beatles" have been added to the bot status

--- cogs/status.py
+++ cogs/status.py
@@ -1,5 +1,4 @@
 from discord.ext import commands, tasks
-from cogs import db
 import discord
 import random
 
@@ -8,29 +7,55 @@ class status(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.songs = [
+            "Metallica - Nothing Else Matters",
+            "Metallica - The Unforgiven",
+            "Metallica - Enter Sandman",
+            "Metallica - Sad But True",
+
+            "Nirvana - Smells Like Teen Spirit",
+            "Nirvana - Heart-Shaped Box",
             "Nirvana - Rape Me",
             "Nirvana - Polly",
             "Nirvana - Dumb",
-            "Nirvana - Heart-Shaped Box",
-            "Nirvana - Smells Like Teen Spirit",
 
+            "Nizkiz - Небяспечна",
+            "Nizkiz - Интроверт",
+            "Nizkiz - Полночь",
+            "Nizkiz - Правiлы",
+            "Nizkiz - Блiзка",
+
+            "Radiohead - Fake Plastic Trees",
+            "Radiohead - High and Dry",
             "Radiohead - Karma Police",
             "Radiohead - No Surprises",
             "Radiohead - Creep",
 
-            "Nizkiz - Небяспечна",
-            "Nizkiz - Правiлы",
-            "Nizkiz - Блiзка",
+            "Sex Pistols - God Save The Queen",
+            "Sex Pistols - Anarchy In The UK",
+            "Sex Pistols - I Wanna Be Me",
+            "Sex Pistols - Bodies",
 
-            "Порнофильмы - Это пройдёт",
+            "System Of A Down - Lost In Hollywood",
+            "System Of A Down - Chop Suey!",
+            "System Of A Down - Lonely Day",
+            "System Of A Down - Toxicity",
+            "System Of A Down - Aerials",
+
+            "The Beatles - Here Comes The Sun",
+            "The Beatles - Yellow Submarine"
+            "The Beatles - Come Together",
+            "The Beatles - Let It Be",
+            "The Beatles - Yesterday",
+
             "Порнофильмы - Россия для грусных",
             "Порнофильмы - Уроки любви",
-            "Порнофильмы - Чужое горе"
+            "Порнофильмы - Это пройдёт",
+            "Порнофильмы - Чужое горе",
             ]
 
         self.changer.start()
 
-    @tasks.loop(minutes=3.0)
+    @tasks.loop(minutes=5.0)
     async def changer(self):
         await self.bot.change_presence(activity=discord.Activity(
             type=discord.ActivityType.listening,
