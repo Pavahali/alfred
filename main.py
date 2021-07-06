6f77f07c8e3fcbb832c4feef43145104557bd4c1
pava <pavahalis@protonmail.ch>
2021-07-06 22:52:47 +0300

    new songs + bugfix

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ client.remove_command('help')
 @client.command()
 async def stats(ctx):
     embed = discord.Embed(title="Stats")
-    embed.add_field(name="Версия", value="v0.5.2", inline=True)
+    embed.add_field(name="Версия", value="v0.6", inline=True)
     embed.add_field(name="Пинг", value=round(ctx.bot.latency, 2) * 100, inline=True)
     embed.add_field(name="Размер nohup", value=f"{round(os.path.getsize('nohup.out') / 1024, 2)} килобайт", inline=True)
     embed.add_field(name="Размер логов", value=f"{round(os.path.getsize('logs.log') / 1024, 2)} килобайт", inline=True)
